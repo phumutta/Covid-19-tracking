adc6bbdb6cba83008c2e4dabd906a1688890590f
Phumutta <Phumutta@gmail.com>
2020-09-20 19:39:18 +0700

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
  import React from 'react'
-
+// import Cards from'./components/Cards/Cards';
+// import Chart from'./components/Chart/Chart';
+// import CountryPicker from'./components/CountryPicker/CountryPicker';
+import {Cards,Chart,CountryPicker} from './components';
+import style from './App.module.css';
+import {fetchData} from './api';
 class App extends React.Component{
-     render(){
+    state={
+        data:{},
 
+    }
+    async componentDidMount (){
+        const fetchdata=await fetchData();
+        this.setState({data:fetchdata});
+        // console.log (data);
+    }
+
+     render(){
+        const {data}=this.state;
         return(
-            <div>
-                <h1>App</h1>
+            <div className ={style.container}>
+                <Cards data={data}/>
+                <CountryPicker/>
+                <Chart/>
             </div>
         )
      }
