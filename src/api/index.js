feca0daa34fde687b13501c81fbea9d875b9a35d
Phumutta <Phumutta@gmail.com>
2020-09-21 00:27:27 +0700

    update fetchDailyData funtion

--- src/api/index.js
+++ src/api/index.js
@@ -4,15 +4,26 @@ const url='https://covid19.mathdro.id/api';
 
 export const fetchData =async ()=>{
     try{
-        const {data} =await axios.get(url);
+        const {data:{confirmed,recovered,deaths,lastUpdate}} =await axios.get(url);
         
-        const modifiedData={
-            confirmed:data.confirmed,
-            recovered:data.recovered,
-            deaths:data.deaths,
-            lastUpdate:data.lastUpdate
-        }
-        return modifiedData;
+        return {confirmed,recovered,deaths,lastUpdate};
+        // const modifiedData={
+        //     confirmed:data.confirmed,
+        //     recovered:data.recovered,
+        //     deaths:data.deaths,
+        //     lastUpdate:data.lastUpdate
+        // }
+        // return modifiedData;
+    }
+    catch(error){
+    
+    }
+}
+
+export const fetchDailyData =async ()=>{
+    try{
+        const {data} =await axios.get(`${url}/daily`);
+        console.log(data);
     }
     catch(error){
 
