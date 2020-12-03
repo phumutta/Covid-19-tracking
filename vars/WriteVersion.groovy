15889132758ecc59dc4218105ff936205514ffc7
Nattapon Phumutta <47663338+phumutta@users.noreply.github.com>
2020-12-04 01:16:58 +0700

    Update WriteVersion.groovy

--- vars/WriteVersion.groovy
+++ vars/WriteVersion.groovy
@@ -1,7 +1,7 @@
 
 def call() {
         sh "mkdir -p Version"
-        writeFile file: "Version/version.txt", text: "This file is useful, need to archive it."
+        writeFile file: "./version.txt", text: "This file is useful, need to archive it."
 
 
     
