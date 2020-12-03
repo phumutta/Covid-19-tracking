1cb5d611f901ba8bc2eca783557833324181df22
Nattapon Phumutta <47663338+phumutta@users.noreply.github.com>
2020-12-04 01:26:07 +0700

    Update WriteVersion.groovy

--- vars/WriteVersion.groovy
+++ vars/WriteVersion.groovy
@@ -2,7 +2,8 @@
 def call(String ver = '1.0.0-10') {
         writeFile file: "./version.txt", text: "Version is:${ver}"
 
-
+         def data = readFile(file: 'version.txt')
+            println(data)
     
                 
 }
